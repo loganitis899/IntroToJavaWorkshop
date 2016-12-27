b6ca5a9fb27a34fa68b39398ed9a3c2a86d3deeb
Stephen Cerruti <scerruti@otabi.com>
2016-12-27 11:56:31 -0800

    day 2 and ninja star

--- src/day1/robot/FourSquare.java
+++ src/day1/robot/FourSquare.java
@@ -6,29 +6,26 @@ import org.jointheleague.graphical.robot.Robot;
 public class FourSquare {
 	
 	// 2. Create a new Robot
-	
+Robot mrPickles=new Robot();	
 
 	void go() {
-		// 4. Make the robot move as fast as possible
-
-		// 5. Set the pen width to 5
-
-		// 6. Do steps #7 to #8 four times...
-
-			// 7. Set the pen color to random
 	
-			// 1. Call the drawSquare() method
-	
-			// 8. Turn the robot 90 degrees to the right
+mrPickles.setSpeed(1000000000);
 
+mrPickles.penDown();
+mrPickles.setPenWidth(5);
+
+int length=300;
 	}
 
 	/* 3. Fill in the code to draw a square inside the method below. */
-	void drawSquare() {
-		JOptionPane.showMessageDialog(null, "yay! you called the drawSquare() method!");
-
-		
-		
+	{mrPickles.setSpeed(1000000000);
+int length=100;
+for (int i=0; i<4; i++){
+	mrPickles.penDown();
+		mrPickles.move(length);
+		mrPickles.turn(90);
+}
 	}
 
 	public static void main(String[] args) {
--- src/day5/FlamingNinjaStar.java
+++ src/day5/FlamingNinjaStar.java
@@ -16,34 +16,38 @@ public class FlamingNinjaStar {
 		
 		// *15. Make some adjustments to see what other kinds of shapes you can make.
 
-		// 1. Make a new robot, and set it's pen down.
-
+		
+Robot turtle=new Robot();
+for(int i=0; i<25; i++){ turtle.penDown();
+turtle.setSpeed(10);
 		// 12. Set the robot speed to 10
 
 		// 13. Make all the code below repeat 25 times
 
-			// 2. Turn the robot 1/8 of a circle
-		
-			// 3. Move the robot 64 pixels
+			
+
+turtle.turn(360/8);
+turtle.move(64);
+turtle.setPenColor(255, 255, 0);
 
 			
-			// 4. Turn the robot 40 degrees to the LEFT. (Negative numbers will turn the robot counter-clockwise.)
+turtle.turn(-40);			
 			
-			// 5. Move the robot the distance in the variable flameSize
+turtle.move(200);
 			
-			// 6. Turn the robot 170 degrees
+turtle.turn(170);
+
+
+turtle.move(200);
 			
-			// 7. Move the robot the distance in the variable flameSize (again)
-			
-			// 8. Turn the robot 64 degrees to the right
-			
-			// 9. Move the robot the distance in the variable baseSize
-			
+turtle.turn(64);
+
+turtle.move(300); 
 			// 10. Check that your shape is the same as Figure 1. This is one arm of the ninja star.
 			// 11. Color your ninja star like Figure 2.
 		
+}
+
 	}
 
 }
-
-
